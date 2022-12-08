a48fbebd95bfa2783bb56a60c1f778e5cee38def
Waya-mst <111881126+Waya-mst@users.noreply.github.com>
2022-12-08 15:27:40 +0900

    add verShader&fragshader

--- main.cpp
+++ main.cpp
@@ -3,6 +3,46 @@
 #include<GL/glew.h>
 #include <GLFW/glfw3.h>
 
+//プログラムオブジェクトを作成する
+//  vsrc: 頂点シェーダのソースプログラムの文字列
+//  fsrc: フラグメントシェーダのソースプログラムの文字列
+GLuint createProgram(const char *vsrc, const char *fsrc)
+{
+    //空のプロブラムオブジェクトを作成
+    const GLuint program(glCreateProgram());
+
+    if (vsrc != NULL)
+    {
+        //頂点シェーダのシェーダオブジェクトを作成する
+        const GLuint vobj(glCreateShader(GL_VERTEX_SHADER));
+        glShaderSource(vobj, 1, &vsrc, NULL);
+        glCompileShader(vobj);
+
+        //頂点シェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+        glAttachShader(program, vobj);
+        glDeleteShader(vobj);
+    }
+
+    if (fsrc != NULL)
+    {
+        //フラグメントシェーダのシェーダオブジェクトを作成する
+        const GLuint fobj(glCreateShader(GL_FRAGMENT_SHADER));
+        glShaderSource(fobj, 1, &fsrc, NULL);
+        glCompileShader(fobj);
+
+        //フラグメントシェーダのシェーダオブジェクトをプログラムオブジェクトに組み込む
+        glAttachShader(program, fobj);
+        glDeleteShader(fobj);
+    }
+
+    //  プログラムオブジェクトをリンクする
+    glBindAttribLocation(program, 0, "position");
+    glBindFragDataLocation(program, 0, "fragment");
+    glLinkProgram(program);
+
+    //作成したプログラムオブジェクトを返す
+    return program;
+}
 int main()
  {
     //GLFWを初期化する
